856f09122814e14b2b7721ac2a6bae1297300377
JarikiSungnyung <hyuk0496@gmail.com>
2026-03-06 14:38:05 +0900

    Correct: BOJ 4153

--- Codes/4153.py
+++ Codes/4153.py
@@ -1,13 +1,20 @@
 inputList = []
+i = 0
 
 while True:
+    
     inputList.append(input())
+    
     if inputList[-1] == '0 0 0':
         break
     
-for i in range(len(inputList)-1):
     a, b, c = map(int, inputList[i].split())
-    if a**2 + b**2 == c**2:
-        print('right')
+    
+    sides = sorted([a, b, c])
+    
+    if sides[0]**2 + sides[1]**2 == sides[2]**2:
+        print("right")
     else:
-        print('wrong')
+        print("wrong")
+        
+    i += 1
